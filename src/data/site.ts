a98c913678120da6d21cb46f4f60e3421c8414e8
Ben Turner <ben@redgames.co>
2026-08-05 10:39:40 -0600

    Making site more user friendly, disabling background build for now

--- src/data/site.ts
+++ src/data/site.ts
@@ -1,10 +1,18 @@
+export const backgroundBuildEnabled = false;
+
 export const smoothOperatorAssetStoreUrl =
   'https://assetstore.unity.com/packages/tools/utilities/smooth-operator-271378';
 
 export const smoothOperatorVideoUrl = 'https://www.youtube.com/embed/cIYW7QI1iFk';
 
 export const navLinks = [
-  { label: 'Background Build', href: '/products/background-build' },
+  ...(backgroundBuildEnabled ? [{ label: 'Background Build', href: '/products/background-build' }] : []),
+  { label: 'Smooth Operator', href: '/products/smoothoperator' },
+  { label: 'Support', href: '/support' },
+];
+
+export const footerLinks = [
+  ...(backgroundBuildEnabled ? [{ label: 'Background Build', href: '/products/background-build' }] : []),
   { label: 'Smooth Operator', href: '/products/smoothoperator' },
   { label: 'Support', href: '/support' },
 ];
